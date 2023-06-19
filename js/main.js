672c18e7df8551ce9d5512dbe12b5220c414e8fa
“milliybro” <shohrux-rustamov@mail.ru>
2023-06-19 09:59:08 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", function () {
+   toggleBacktop();
+ });
+ 
+ let backtop = document.getElementById("backtop");
+ 
+ function toggleBacktop() {
+   if (
+     document.body.scrollTop > 200 ||
+     document.documentElement.scrollTop > 200
+   ) {
+     backtop.classList.add('backtop-show')
+   } else {
+     backtop.classList.remove('backtop-show')
+   }
+ }
+
+ 
+ function openNavbar() {
+   document.getElementById("navbar-responsive").style.left = "0";
+   // document.getElementById("navbar-responsive").style.top = "0";
+ }
+ function closeNavbar() {
+   document.getElementById("navbar-responsive").style.left = "-100%";
+   // document.getElementById("navbar-responsive").style.top = "-100%";
+ }
+ 
+ document.getElementById("navbar-open").addEventListener("click", openNavbar);
+ document.getElementById("navbar-close").addEventListener("click", closeNavbar);
